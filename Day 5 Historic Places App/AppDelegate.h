74fb0b6c0aa8f00f182560ae52138a2e0b743263
Daniyar Mukhanov <mukhanov.daniyar@gmail.com>
2015-06-24 09:53:36 +0600

    Initial Commit

---
+++ Day 5 Historic Places App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day 5 Historic Places App
+//
+//  Created by Daniyar Mukhanov on 6/24/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Day 5 Historic Places App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day 5 Historic Places App
+//
+//  Created by Daniyar Mukhanov on 6/24/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
