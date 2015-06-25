eca699220ba6ffaaf3e63e46b25709b3c4babd7f
Daniyar Mukhanov <mukhanov.daniyar@gmail.com>
2015-06-25 09:53:45 +0600

    addplace

---
+++ Day 5 Historic Places App/AddPlaceViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddPlaceViewController.h
+//  Day 5 Historic Places App
+//
+//  Created by Daniyar Mukhanov on 6/24/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AddPlaceViewController : ViewController
+
+@end
---
+++ Day 5 Historic Places App/AddPlaceViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddPlaceViewController.m
+//  Day 5 Historic Places App
+//
+//  Created by Daniyar Mukhanov on 6/24/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import "AddPlaceViewController.h"
+
+@interface AddPlaceViewController ()
+
+@end
+
+@implementation AddPlaceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
